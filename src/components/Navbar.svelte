00f36460e8a5eca1996abba978f947083b20f2ca
Alan <john1337@gmail.com>
2022-04-13 20:02:54 -0500

    Ruta-productos

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,8 @@
 <div class="header">
 	<h1>Svelte JS CRUD</h1>
 	<nav>
-		<a href="productos">Inicio</a>
+		<a href="/">CRUD</a>
+		<a href="productos">Productos</a>
 	</nav>
 </div>
 
@@ -13,7 +14,12 @@
 	}
 	nav a {
 		text-decoration: none;
+		padding: 10px;
 		color: #000;
 		font-size: 20px;
+		font-weight: bold;
+	}
+	a:hover {
+		color: #083897;
 	}
 </style>
--- src/routes/productos.svelte
+++ src/routes/productos.svelte
@@ -12,20 +12,44 @@
 
 <h2>Productos</h2>
 
-<div>
+<div class="contenedor-frutas">
 	{#each productos as { id, nombre, precio, sku, temporada, imagen }}
-		<div class="contenedor-frutas">
-			<img src={imagen} alt="" />
-			<p>{sku}</p>
-			<h3>{nombre}</h3>
-			<p>{precio}</p>
-			<p>{temporada}</p>
+		<div class="card">
+			<div class="card__cover"><img src={imagen} alt="" /></div>
+
+			<div class="card__content">
+				<h3>Producto: {nombre}</h3>
+				<p><span class="descripcion">SKU:</span> {sku}</p>
+			</div>
+			<p><span class="descripcion">En temporada:</span>{temporada}</p>
+			<p><span class="descripcion">Precio:$ </span>{precio}</p>
 		</div>
 	{/each}
 </div>
 
 <style>
-	.contenedor-frutas {
-		display: grid;
+	img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+
+	.card {
+		border: 1px solid #ccc;
+		border-radius: 10px;
+		padding: 20px;
+	}
+	.card__content {
+		text-align: center;
+	}
+	.descripcion {
+		font-size: 15px;
+		font-weight: bold;
+	}
+	@media (min-width: 768px) {
+		.contenedor-frutas {
+			display: grid;
+			grid-template-columns: repeat(4, 1fr);
+			gap: 40px;
+		}
 	}
 </style>
